08592b6eade1fed9f937a6e63c57bd29e016bad1
方永乐 <fangyongle@innobuddy.com>
2018-02-01 10:07:51 +0800

    chore: 更新gulp插件函数名称

--- gulp-plugin-add-less-import.js
+++ gulp-plugin-add-less-import.js
@@ -2,9 +2,9 @@ const path = require('path');
 const through = require('through-gulp');
 
 // exporting the plugin
-module.exports = iconGeneration;
+module.exports = addLessImport;
 
-function iconGeneration({ themePath, commomPath }) {
+function addLessImport({ themePath, commomPath }) {
   // creating a stream through which each file will pass
   return through(function(file, encoding, callback) {
     let html = file.contents.toString('utf-8');
